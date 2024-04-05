d08c8e4cd24f4343b9f312adc6509eafee7e9529
unknown <guigui0246@yahoo.com>
2024-04-05 19:10:07 +0200

    [ADD] add screen

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 import pygame
 
+SIZE: tuple[int, int] = (1280, 720)
+size: tuple[int, int] = SIZE
+
 
 def main():
+    global size
     pygame.init()
-    while not pygame.event.get().type == pygame.QUIT:
-        pass
+    pygame.display.set_caption("Open")
+    screen: pygame.Surface = pygame.display.set_mode(size, pygame.RESIZABLE)
+    clock: pygame.time = pygame.time.Clock()
+    while not len(list(filter(lambda a: a.type == pygame.QUIT, pygame.event.get()))):
+        size = screen.get_size()
+        screen.fill("white")
+        pygame.display.flip()
+        clock.tick(60)
     pygame.quit()
     pass
 
